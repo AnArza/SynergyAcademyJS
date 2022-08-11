454d57b023d31458f8b969c029df6af6bbd7fad6
AnArza <54543215+AnArza@users.noreply.github.com>
2022-08-11 23:52:20 +0400

    get function name

---
+++ Day2/Task6.js
@@ -0,0 +1,10 @@
+function getFuncName(func){
+    return `The function's name is ${func.name}`;
+}
+
+function HaykanushFunc(){}
+function OtherFunc(){}
+function ArtavazdFunc(){}
+let MekhakFunc=function(){}
+
+console.log(getFuncName(MekhakFunc));
