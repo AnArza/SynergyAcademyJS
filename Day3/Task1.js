e82c33a07ada9ecb20b34f8ade3677ff957d17a7
AnArza <54543215+AnArza@users.noreply.github.com>
2022-08-14 13:49:41 +0400

    merge sort

--- Day3/Task1.js
+++ Day3/Task1.js
@@ -11,9 +11,9 @@ function merge(left,right){
     }
     
     if(left.length==0 && right.length!=0){
-        merged.push(...right);
-    } else {
-        merged.push(...left);
+        merged.push(right[0]);
+    } else if(left.length!=0 && right.length==0){
+        merged.push(left[0]);
     }
     return merged;
 }
